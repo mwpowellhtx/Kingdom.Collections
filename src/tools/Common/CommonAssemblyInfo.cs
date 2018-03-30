91cbb87d6fa7746f1babc851007efa7d55edc410
Michael W Powell <mwpowellhtx@gmail.com>
2018-03-30 11:33:54 -0400

    refactored common assembly info

---
+++ src/tools/Common/CommonAssemblyInfo.cs
@@ -0,0 +1,8 @@
+using System.Reflection;
+
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Kingdom Software")]
+[assembly: AssemblyProduct("Kingdom.Collections")]
+[assembly: AssemblyCopyright("Copyright © 2016, 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
