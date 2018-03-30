193b0ffcbfb73d87b1e662c4ab33700981d4a566
Michael W Powell <mwpowellhtx@gmail.com>
2018-03-30 11:35:26 -0400

    bumped assembly and package versions

--- src/Kingdom.Collections.ImmutableBitArray/Properties/AssemblyInfo.cs
+++ src/Kingdom.Collections.ImmutableBitArray/Properties/AssemblyInfo.cs
@@ -1,5 +1,4 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
@@ -7,12 +6,6 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 [assembly: AssemblyTitle("Kingdom.Collections.ImmutableLibrary")]
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Kingdom Software")]
-[assembly: AssemblyProduct("Kingdom.Collections")]
-[assembly: AssemblyCopyright("Copyright © 2016")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -32,5 +25,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.5.1.*")]
-[assembly: AssemblyFileVersion("1.5.1")]
+[assembly: AssemblyVersion("1.5.2.*")]
+[assembly: AssemblyFileVersion("1.5.2")]
