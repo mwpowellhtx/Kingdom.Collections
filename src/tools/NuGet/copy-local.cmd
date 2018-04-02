2998105e2093f13dc390c62e657ffc6df2a6826a
Michael W Powell <mwpowellhtx@gmail.com>
2018-04-02 15:26:50 -0400

    added copy local scripting added package scripting

---
+++ src/tools/NuGet/copy-local.cmd
@@ -0,0 +1,37 @@
+@echo off
+
+setlocal
+
+:: set local_dir=C:\Dev\NuGet\packages
+set local_dir=G:\Dev\NuGet\local\packages
+set nupkg_ext=.nupkg
+
+::
+pushd packages
+
+call :publishpackage Kingdom.Collections.ImmutableBitArray
+
+popd
+
+goto :end
+
+:publishpackage
+set f=%1
+set package_path=%f%.*%nupkg_ext%
+if not exist "%package_path%" (
+    echo Package '%package_path%' not found.
+)
+:: We need to scan beyond the wildcard package path here and get to specifics.
+for %%x in ("%package_path%") do (
+    if not exist "%local_dir%" mkdir "%local_dir%"
+    :: Make sure that we do not inadvertently overwrite already existing package versions.
+    if not exist "%local_dir%\%%x" echo Moving '%%x' package to local directory '%local_dir%'...
+    if not exist "%local_dir%\%%x" move /Y "%%x" "%local_dir%"
+)
+exit /b
+
+:end
+
+endlocal
+
+pause
---
+++ src/tools/NuGet/package.cmd
@@ -0,0 +1,35 @@
+@echo off
+
+setlocal
+
+set packages_path=tools\NuGet\packages
+set nuget_exe=NuGet.exe
+set csproj_ext=.csproj
+set nupkg_ext=.nupkg
+
+pushd ..\..
+
+call :packproject Kingdom.Collections.ImmutableBitArray
+
+popd
+
+goto :end
+
+:packproject
+set f=%1
+set csproj_path=%f%\%f%%csproj_ext%
+set package_path=%f%.*%nupkg_ext%
+if exist "%csproj_path%" (
+    %nuget_exe% pack "%csproj_path%" -Build -Properties Configuration=Release
+)
+if exist "%package_path%" (
+    if not exist "%packages_path%" mkdir "%packages_path%"
+    move /Y "%package_path%" "%packages_path%"
+)
+exit /b
+
+:end
+
+endlocal
+
+pause
