4a66fbbcc65743a6d243efd9311c2d4a0d4f7e1c
Michael W Powell <mwpowellhtx@gmail.com>
2018-05-14 12:30:50 -0400

    so mostly changes were to the project file making the transition into dot net standard packaging bits are now incorporated into the project file will keep hold of the spec in the interim and in the event we could eventually run with the spec itself also incorporated usage of a design time only version bumping package

--- src/Kingdom.Collections.Queues/Properties/AssemblyInfo.cs
+++ src/Kingdom.Collections.Queues/Properties/AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("d1f98bc1-ba49-4f1f-a908-896d146f43c4")]
+[assembly: Guid("f0dd2e97-32c8-4281-8abb-138dee4b9984")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -25,5 +25,4 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.*")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
