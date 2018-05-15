d6ff11e2bca309cd7e369c9160e43d8430eeb845
Michael W Powell <mwpowellhtx@gmail.com>
2018-05-15 09:43:45 -0400

    bumped assembly version etc

--- src/Kingdom.Collections.DataStructures.Tests/Properties/AssemblyInfo.cs
+++ src/Kingdom.Collections.DataStructures.Tests/Properties/AssemblyInfo.cs
@@ -1,29 +1,29 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Kingdom.Collections.DataStructures.Tests")]
 [assembly: AssemblyDescription("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("800cbc76-b0cc-4839-9f54-18e562066341")]
+[assembly: Guid("1c57af29-1edd-4484-874d-9e2c4303bed7")]
 
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version 
+//      Minor Version
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.1.0")]
-[assembly: AssemblyFileVersion("1.0.1.0")]
+[assembly: AssemblyVersion("1.0.2.*")]
+[assembly: AssemblyFileVersion("1.0.2.0")]
