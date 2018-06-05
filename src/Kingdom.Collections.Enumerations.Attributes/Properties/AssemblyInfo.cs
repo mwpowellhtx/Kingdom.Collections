50435a64922ff74274099a9b4a8d095a6aa0cb58
Michael W Powell <mwpowellhtx@gmail.com>
2018-06-05 14:09:10 -0400

    transitioning away from assembly informational bits toward project informational bits

--- src/Kingdom.Collections.Enumerations.Attributes/Properties/AssemblyInfo.cs
+++ src/Kingdom.Collections.Enumerations.Attributes/Properties/AssemblyInfo.cs
@@ -1,11 +1,4 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Kingdom.Collections.Enumerations.Attributes")]
-[assembly: AssemblyDescription("")]
+﻿using System.Runtime.InteropServices;
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -15,14 +8,3 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("fff60283-ff8c-445a-b76b-7a4f770da949")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
