ad62d6f64cd020c8f823b2a870e1ab69edbe22bf
Michael W Powell <mwpowellhtx@gmail.com>
2018-06-05 13:20:10 -0400

    transitioning away from assembly information bits to project driven informational bits also marks the point at which starting to use a more robust versioning mechanism the only down side is that it is a studio extension and not a private assets package but this potentially yields an opportunity to do something special

---
+++ src/Kingdom.Collections.Enumerations.Analyzers.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,21 @@
+﻿using System.Runtime.InteropServices;
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d41de699-3884-4921-814a-e5bd85ea3500")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+// See: http://stackoverflow.com/questions/43019832/auto-versioning-in-visual-studio-2017-net-core#answer-46985624
