344dbaf252ff7f7cd6bbef9eee969ec6269203a4
Michael W Powell <mwpowellhtx@gmail.com>
2018-06-05 14:00:55 -0400

    transitioning away from assembly informational and toward project informational bits

--- src/Kingdom.Collections.Enumerations.Analyzers/Properties/AssemblyInfo.cs
+++ src/Kingdom.Collections.Enumerations.Analyzers/Properties/AssemblyInfo.cs
@@ -1,12 +1,5 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
+﻿using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Kingdom.Collections.Enumerations.Analyzers")]
-[assembly: AssemblyDescription("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -26,6 +19,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
 
 [assembly: InternalsVisibleTo("Kingdom.Collections.Enumerations.Analyzers.Tests")]
