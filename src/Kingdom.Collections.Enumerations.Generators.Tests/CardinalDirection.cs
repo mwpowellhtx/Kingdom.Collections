21a38e33e20badcb1be4d61d167aaacedfaa87e2
Michael W Powell <mwpowellhtx@gmail.com>
2018-06-18 16:04:37 -0400

    removed comments from cardinal direction in situational subject

--- src/Kingdom.Collections.Enumerations.Generators.Tests/CardinalDirection.cs
+++ src/Kingdom.Collections.Enumerations.Generators.Tests/CardinalDirection.cs
@@ -1,21 +1,26 @@
-﻿//// TODO: TBD: this was an interesting idea, but I think we lack the proper context in order to do something like this at this level...
-//namespace Kingdom.Collections
-//{
-//    [FlagsEnumeration]
-//    public partial class CardinalDirection : Enumeration<CardinalDirection>
-//    {
-//        public static readonly CardinalDirection North = new CardinalDirection();
+﻿namespace Kingdom.Collections
+{
+    /// <summary>
+    /// We will expect that actual Code Generation occurs for this definition.
+    /// Which, there should be artifacts of that Code Generation vetted via
+    /// <see cref="InSituFlagsEnumerationCodeGenerationTestsBase{T}"/>.
+    /// </summary>
+    /// <inheritdoc />
+    [FlagsEnumeration]
+    public partial class CardinalDirection : Enumeration<CardinalDirection>
+    {
+        public static readonly CardinalDirection North = new CardinalDirection();
 
-//        public static readonly CardinalDirection East = new CardinalDirection();
+        public static readonly CardinalDirection East = new CardinalDirection();
 
-//        public static readonly CardinalDirection South = new CardinalDirection();
+        public static readonly CardinalDirection South = new CardinalDirection();
 
-//        public static readonly CardinalDirection West = new CardinalDirection();
+        public static readonly CardinalDirection West = new CardinalDirection();
 
-//        static CardinalDirection() => InitializeBits(Values);
+        static CardinalDirection() => InitializeBits(Values);
 
-//        private CardinalDirection()
-//        {
-//        }
-//    }
-//}
+        private CardinalDirection()
+        {
+        }
+    }
+}
