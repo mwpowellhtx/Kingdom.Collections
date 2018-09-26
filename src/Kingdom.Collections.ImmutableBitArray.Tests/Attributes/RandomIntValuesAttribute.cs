342181ded9d032e42d883b1e6e20fbc7c9563e29
Michael W Powell <mwpowellhtx@gmail.com>
2018-09-26 09:23:56 -0400

    changed up the strategies in which random values are provided slightly now supports not only attribution but also direct static usage when necessary in other places

--- src/Kingdom.Collections.ImmutableBitArray.Tests/Attributes/RandomIntValuesAttribute.cs
+++ src/Kingdom.Collections.ImmutableBitArray.Tests/Attributes/RandomIntValuesAttribute.cs
@@ -8,18 +8,26 @@ namespace Kingdom.Collections
 
     public class RandomIntValuesAttribute : CombinatorialValuesAttribute
     {
+        // TODO: TBD: not sure how much Lazy is hurting/helping here...
         private static readonly Lazy<Random> LazyRnd = new Lazy<Random>(
             () => new Random((int) DateTime.UtcNow.Ticks % int.MaxValue));
 
         private static Random Rnd => LazyRnd.Value;
 
-        private static IEnumerable<object> GetRandomValues()
+        /// <summary>
+        /// Gets the Random Values for Internal use.
+        /// </summary>
+        internal static IEnumerable<uint> InternalRandomValues
         {
-            return Enumerable.Range(0, 4).Select(_ => (uint) Rnd.Next()).Cast<object>();
+            get
+            {
+                IEnumerable<uint> GetAll(params uint[] values) => values;
+                return GetAll((uint) Rnd.Next(), (uint) Rnd.Next(), (uint) Rnd.Next()).ToArray();
+            }
         }
 
         private static Lazy<object[]> LazyValues { get; }
-            = new Lazy<object[]>(() => GetRandomValues().ToArray());
+            = new Lazy<object[]>(() => InternalRandomValues.Cast<object>().ToArray());
 
         internal RandomIntValuesAttribute()
             : base(LazyValues.Value)
