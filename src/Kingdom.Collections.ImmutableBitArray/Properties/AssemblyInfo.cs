2610eac172f84b9bed9932288340647dddd7f07f
Michael W Powell <mwpowellhtx@gmail.com>
2018-09-26 09:17:52 -0400

    exposed internals to the unit tests

--- src/Kingdom.Collections.ImmutableBitArray/Properties/AssemblyInfo.cs
+++ src/Kingdom.Collections.ImmutableBitArray/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.Runtime.InteropServices;
+﻿using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -7,3 +8,5 @@
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("63efbcdf-d549-4368-ac94-7804ed32717e")]
+
+[assembly: InternalsVisibleTo("Kingdom.Collections.ImmutableBitArray.Tests")]
