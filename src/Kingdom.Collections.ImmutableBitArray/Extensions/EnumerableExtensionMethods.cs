d0cc2d7e0acb33dc24227fe957ad1759052fd0b3
Michael W Powell <mwpowellhtx@gmail.com>
2018-09-26 09:18:31 -0400

    added an extension method however not certain will be needing it after all

---
+++ src/Kingdom.Collections.ImmutableBitArray/Extensions/EnumerableExtensionMethods.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Kingdom.Collections
+{
+    internal static class EnumerableExtensionMethods
+    {
+        public static IEnumerable<T> ReverseTake<T>(this IEnumerable<T> values, int count = 0)
+        {
+            var i = 0;
+
+            while (i++ < count)
+            {
+                // ReSharper disable once PossibleMultipleEnumeration
+                yield return values.ElementAt(
+                    // ReSharper disable once PossibleMultipleEnumeration
+                    values.Count() - i - 1);
+            }
+        }
+    }
+}
