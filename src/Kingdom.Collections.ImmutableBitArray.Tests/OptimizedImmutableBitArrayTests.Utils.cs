2fb18ffb98c8af17dff804b36c1f1b109f181f3b
Michael W Powell <mwpowellhtx@gmail.com>
2018-09-26 09:27:43 -0400

    added the beginnings of optimized bit array tests including several utilities and data members

---
+++ src/Kingdom.Collections.ImmutableBitArray.Tests/OptimizedImmutableBitArrayTests.Utils.cs
@@ -0,0 +1,10 @@
+﻿namespace Kingdom.Collections
+{
+    // ReSharper disable once UnusedMember.Global
+    public partial class OptimizedImmutableBitArrayTests
+    {
+        // TODO: TBD: establish tests saying we can create the expected bit array using the ctors ...
+
+        private static OptimizedImmutableBitArray CreateBitArray(uint x) => new OptimizedImmutableBitArray(x);
+    }
+}
