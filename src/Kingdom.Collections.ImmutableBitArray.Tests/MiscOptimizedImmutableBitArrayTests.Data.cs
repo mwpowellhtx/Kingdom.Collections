34c83be8614c8090c05b889efdbc91632b2d5941
Michael W Powell <mwpowellhtx@gmail.com>
2018-09-26 13:08:22 -0400

    added length and count unit tests including corresponding member data

---
+++ src/Kingdom.Collections.ImmutableBitArray.Tests/MiscOptimizedImmutableBitArrayTests.Data.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Kingdom.Collections
+{
+    using static DateTime;
+
+    // ReSharper disable once UnusedMember.Global
+    public partial class MiscOptimizedImmutableBitArrayTests
+    {
+        private static Random Rnd { get; } = new Random((int) (UtcNow.Ticks % int.MaxValue));
+
+        private static IEnumerable<object[]> _lengthCountData;
+
+        public static IEnumerable<object[]> LengthCountData
+        {
+            get
+            {
+                IEnumerable<object[]> GetAll()
+                {
+                    IEnumerable<object> GetOne(IEnumerable<byte> bytes, int lengthDelta)
+                    {
+                        yield return GetRange(bytes.ToArray());
+                        yield return lengthDelta;
+                    }
+
+                    IEnumerable<byte> GetBytes(int count)
+                    {
+                        while (count-- > 0)
+                        {
+                            yield return (byte) (Rnd.Next() % byte.MaxValue);
+                        }
+                    }
+
+                    const byte maxByte = byte.MaxValue;
+
+                    for (var byteCount = 0; byteCount < sizeof(uint); byteCount++)
+                    {
+                        /* TODO: TBD: furnishing Prime Numbers here is completely arbitrary; I
+                         * just want something that is guaranteed not to coincide with a bit/byte
+                         * boundary for test purposes. */
+
+                        /* Poor man's range of Prime Numbers for test purposes. Could be any
+                         * range, just as long as it exercises exceeding the width of the Bits in
+                         * a Byte, and then some, for test purposes. */
+
+                        // Additionally, Zero (no-change) is especially just as valid a thing to verify.
+                        foreach (var lengthDelta in GetRange(0, 1, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37))
+                        {
+                            // Starting with some Random Test Cases.
+                            yield return GetOne(GetBytes(byteCount), lengthDelta).ToArray();
+                            yield return GetOne(GetBytes(byteCount), -lengthDelta).ToArray();
+
+                            // But let us also better verify that boundary bytes are masked correctly.
+                            yield return GetOne(GetRange(new byte[byteCount].Select(_ => maxByte).ToArray()), lengthDelta).ToArray();
+                            yield return GetOne(GetRange(new byte[byteCount].Select(_ => maxByte).ToArray()), -lengthDelta).ToArray();
+                        }
+                    }
+                }
+
+                return _lengthCountData ?? (_lengthCountData = GetAll().ToArray());
+            }
+        }
+    }
+}
--- src/Kingdom.Collections.ImmutableBitArray.Tests/MiscOptimizedImmutableBitArrayTests.cs
+++ src/Kingdom.Collections.ImmutableBitArray.Tests/MiscOptimizedImmutableBitArrayTests.cs
@@ -1,10 +1,14 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Kingdom.Collections
 {
     using Xunit;
     using Xunit.Abstractions;
     using static BitConverter;
+    using static OptimizedImmutableBitArray;
+    using static String;
 
     public partial class MiscOptimizedImmutableBitArrayTests : SubjectTestFixtureBase<OptimizedImmutableBitArray>
     {
@@ -70,6 +74,83 @@ namespace Kingdom.Collections
             Assert.Equal(expectedY, b.ToBytes(isBigEndian));
             Assert.NotEqual(a.ToBytes(), b.ToBytes());
             Assert.False(a.Equals(b));
+        }
+
+        /// <summary>
+        /// Verifies that the Length and Count respond properly to <paramref name="bytes"/>
+        /// Initialization as well as to changes in
+        /// <see cref="OptimizedImmutableBitArray.Length"/> via <paramref name="lengthDelta"/>.
+        /// </summary>
+        /// <param name="bytes"></param>
+        /// <param name="lengthDelta">Permits changes to
+        /// <see cref="OptimizedImmutableBitArray.Length"/> allowing it to either expand or
+        /// contract accordingly, which may also yield <see cref="ArgumentOutOfRangeException"/>
+        /// when excessive negative delta values are provided.</param>
+        /// <remarks>We could ostensibly separate this into several more focused individual unit
+        /// tests, but we can just as easily leverage the differences in furnished
+        /// <see cref="MemberDataAttribute"/>.</remarks>
+        [Theory, MemberData(nameof(LengthCountData))]
+        public void Length_and_Count_work_correctly(IEnumerable<byte> bytes, int lengthDelta)
+        {
+            Assert.NotNull(bytes);
+            bytes = bytes.ToArray();
+            GetSubject(() => CreateBitArrayWithArray(bytes.ToArray()));
+
+            // Let's make some helpful reports during the unit test for post-mortem analysis.
+            OutputHelper.WriteLine(
+                "Original bytes were: "
+                + $"{Join(", ", Subject.InternalBytes().Select(b => $"'{b:X2}'"))}"
+            );
+
+            var initialExpectedLengthAndCount = bytes.Count() * BitCount;
+            Assert.Equal(initialExpectedLengthAndCount, Subject.Length);
+            Assert.Equal(initialExpectedLengthAndCount, Subject.Count);
+
+            var expectedLength = Subject.Length + lengthDelta;
+            var expectedCount = (expectedLength % BitCount == 0
+                                    ? expectedLength / BitCount
+                                    : expectedLength / BitCount + 1) * BitCount;
+
+            if (expectedLength < 0)
+            {
+                Assert.Throws<ArgumentOutOfRangeException>(() => Subject.Length += lengthDelta)
+                    .WithExceptionDetail(aoorex =>
+                    {
+                        OutputHelper.WriteLine($"Expected {nameof(ArgumentOutOfRangeException)} was thrown: {aoorex}");
+                        // Capture the value and leverage its nameof at the same time.
+                        var value = expectedLength;
+                        Assert.Equal(nameof(value), aoorex.ParamName);
+                        Assert.Equal(value, aoorex.ActualValue);
+                    });
+
+                return;
+            }
+
+            // This is the money shot right here.
+            Subject.Length += lengthDelta;
+            Assert.Equal(expectedLength, Subject.Length);
+            Assert.Equal(expectedCount, Subject.Count);
+
+            // ReSharper disable once InvertIf do not invert this since we use bitIndex subsequently.
+            // Last but not least verify that any Bits on the Boundary Byte exceeding Length are reset.
+            if (Subject.Length % BitCount is int bitPosition && bitPosition != 0)
+            {
+                var boundaryIndex = Subject.Length / BitCount;
+                var actualBoundaryByte = Subject.InternalBytes().ElementAt(boundaryIndex);
+
+                OutputHelper.WriteLine(
+                    $"New Length '{Subject.Length}' occurs within Byte Boundary '{boundaryIndex}'"
+                    + $" at bit index '{bitPosition - 1}' with unmasked byte '{actualBoundaryByte:X2}'."
+                );
+
+                /* So this may not be so Kosher, but there are only so many ways
+                 * to Makean appropriate Mask in the problem domain. */
+
+                // BitPosition, not to be confused with the actual BitIndex.
+                var boundaryByteMask = MakeMask(0, bitPosition - 1);
+                const byte expectedMaskedByte = default(byte);
+                Assert.Equal(expectedMaskedByte, actualBoundaryByte & (byte) ~boundaryByteMask);
+            }
         }
     }
 }
