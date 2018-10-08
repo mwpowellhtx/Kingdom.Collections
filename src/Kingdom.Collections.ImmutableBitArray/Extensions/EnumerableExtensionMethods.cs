cda75abeee32c98af8ff203d0829ccfb7a1800dd
Michael W Powell <mwpowellhtx@gmail.com>
2018-10-08 19:17:40 -0400

    added some extension methods

--- src/Kingdom.Collections.ImmutableBitArray/Extensions/EnumerableExtensionMethods.cs
+++ src/Kingdom.Collections.ImmutableBitArray/Extensions/EnumerableExtensionMethods.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Kingdom.Collections
@@ -15,6 +16,24 @@ namespace Kingdom.Collections
                 yield return values.ElementAt(
                     // ReSharper disable once PossibleMultipleEnumeration
                     values.Count() - i - 1);
+            }
+        }
+
+        [Obsolete("Will probably remove this but keeping it as a reference for the time being")]
+		public static IEnumerable<T> TakeFromBack<T>(this IEnumerable<T> values, int count = 0)
+        {
+            // ReSharper disable once PossibleMultipleEnumeration
+            if (count > values.Count())
+            {
+                yield break;
+            }
+
+            // ReSharper disable once PossibleMultipleEnumeration
+            foreach (var value in values
+                // ReSharper disable once PossibleMultipleEnumeration
+                .Skip(values.Count() - count))
+            {
+                yield return value;
             }
         }
     }
