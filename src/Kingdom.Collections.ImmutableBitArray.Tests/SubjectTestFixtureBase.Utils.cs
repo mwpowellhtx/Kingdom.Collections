523729c47bb253b66a55dac5c436ae4e556bd1cb
Michael W Powell <mwpowellhtx@gmail.com>
2018-10-12 13:47:28 -0400

    return the iterated values instead of the parameter itself this is more accurate to the intended behavior catching up for the renamed collection as well

--- src/Kingdom.Collections.ImmutableBitArray.Tests/SubjectTestFixtureBase.Utils.cs
+++ src/Kingdom.Collections.ImmutableBitArray.Tests/SubjectTestFixtureBase.Utils.cs
@@ -8,17 +8,24 @@ namespace Kingdom.Collections
 
     public abstract partial class SubjectTestFixtureBase<T>
     {
-        protected static IEnumerable<TItem> GetRange<TItem>(params TItem[] values) => values;
+        protected static IEnumerable<TItem> GetRange<TItem>(params TItem[] values)
+        {
+            // ReSharper disable once LoopCanBeConvertedToQuery
+            foreach (var value in values)
+            {
+                yield return value;
+            }
+        }
 
         // TODO: TBD: this is a bit self-defeating? in the sense I am potentially re-writing the code under test? and/or maybe a method, extension, should be exposed internally?
         protected static IEnumerable<byte> GetEndianAwareBytes(uint x)
             => IsLittleEndian ? GetBytes(x) : GetBytes(x).Reverse();
 
-        protected static OptimizedImmutableBitArray CreateBitArray(params uint[] values)
-            => new OptimizedImmutableBitArray(values);
+        protected static ImmutableBitArray CreateBitArray(params uint[] values)
+            => new ImmutableBitArray(values);
 
-        protected static OptimizedImmutableBitArray CreateBitArrayWithArray(params byte[] bytes)
-            => new OptimizedImmutableBitArray(bytes);
+        protected static ImmutableBitArray CreateBitArrayWithArray(params byte[] bytes)
+            => new ImmutableBitArray(bytes);
 
         /// <summary>
         /// Pretty straightforward. Perform the <paramref name="action"/> When
