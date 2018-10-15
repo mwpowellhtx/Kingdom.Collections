1018380cc577b6c1462e082966b137520208e0e7
Michael W Powell <mwpowellhtx@gmail.com>
2018-10-15 17:34:53 -0400

    updated XUnit to latest versions

--- src/Kingdom.Collections.ImmutableBitArray.Tests/Properties/AssemblyInfo.cs
+++ src/Kingdom.Collections.ImmutableBitArray.Tests/Properties/AssemblyInfo.cs
@@ -26,5 +26,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 /* Would have been a major 2.0 notwithstanding XUnit/Combinatorial snafus, on many fronts */
-[assembly: AssemblyVersion("1.5.1.20")]
-[assembly: AssemblyFileVersion("1.5.1.20")]
+[assembly: AssemblyVersion("1.5.1.21")]
+[assembly: AssemblyFileVersion("1.5.1.21")]
