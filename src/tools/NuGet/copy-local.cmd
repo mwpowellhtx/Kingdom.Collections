e9adf23f88df0744aa11b4e683741e42ad024ec8
Michael W Powell <mwpowellhtx@gmail.com>
2018-10-20 14:57:57 -0400

    the primary objective of these updates is so that we may work with a select few or all assemblies as required added background comments concerning JetBrains break downs and following up accordingly added default project variable which informs the all projects option updated from empty string conditions to defined and not defined conditions updated label naming conventions a bit applying for loop strategy to iterate lists of items updating to a more reliable commenting style

--- src/tools/NuGet/copy-local.cmd
+++ src/tools/NuGet/copy-local.cmd
@@ -2,58 +2,123 @@
 
 setlocal
 
-:setvars
-:: set local_dir=C:\Dev\NuGet\packages
+rem TODO: TBD: the R# xUnit testing saga is as of this moment still unresolved
+rem TODO: TBD: specifically concerning treatment of user furnished xUnit settings
+rem TODO: TBD: working to move past this using Visual Studio Test Explorer, console runner, etc
+rem TODO: TBD: although it would be very good to have that capability in sound working order from the jetbrains folks sooner than later
+
+:set_vars
+rem set local_dir=C:\Dev\NuGet\packages
 set local_dir=G:\Dev\NuGet\local\packages
 set nupkg_ext=.nupkg
 
-:setconfig
-set config=%1
-if ("%config%" === "") (
+rem Prepare for delimited list of Projects.
+if not defined delim (
+    set delim=,
+)
+
+:set_projects
+set projects=
+set availableprojects=Kingdom.CodeAnalysis.Verification
+set availableprojects=%availableprojects%%delim%Kingdom.Collections.Enumerations.Attributes
+set availableprojects=%availableprojects%%delim%Kingdom.Collections.Enumerations.Analyzers
+set availableprojects=%availableprojects%%delim%Kingdom.Collections.Enumerations.Generators
+set availableprojects=%availableprojects%%delim%Kingdom.Collections.Enumerations.BuildTime
+set availableprojects=%availableprojects%%delim%Kingdom.CodeAnalysis.Verifiers.CodeFixes
+set availableprojects=%availableprojects%%delim%Kingdom.CodeAnalysis.Verifiers.Diagnostics
+set availableprojects=%availableprojects%%delim%Kingdom.Collections.ImmutableBitArray
+set availableprojects=%availableprojects%%delim%Kingdom.Collections.Enumerations
+set availableprojects=%availableprojects%%delim%Kingdom.Collections.Enumerations.Tests
+set availableprojects=%availableprojects%%delim%Kingdom.Collections.Stacks
+set availableprojects=%availableprojects%%delim%Kingdom.Collections.Queues
+set availableprojects=%availableprojects%%delim%Kingdom.Collections.Deques
+
+:parse_args
+
+if "%1" == "" (
+    goto :end_args
+)
+
+:set_config
+if "%1" == "--config" (
+    shift
+    set config=%1
+    goto :next_args
+)
+
+:add_project
+if "%1" == "--proj" (
+    shift
+    if not defined projects (
+        set projects=%2
+    ) else (
+        set projects=%projects%%delim%%2
+    )
+    goto :next_args
+)
+
+:all_projects
+if "%1" == "--all" (
+    rem Leverage the same :appendprojects that we used receiving individually specified projects.
+    set projects=%availableprojects%
+	goto :next_args
+)
+
+:next_args
+shift
+
+goto :parse_args
+
+:end_args
+
+:verify_args
+
+:verify_projects
+if not defined projects (
+    rem In which case, there is nothing else to do.
+    goto :end
+)
+
+:verify_config
+if not defined config (
     set config=Release
 )
 
-:: Do the main areas here.
+:process_projects
+rem Do the main areas here.
 pushd ..\..
-:: pushd packages
 
-:: This is an interesting package for local consumption, but I do not think
-:: it should be published in a broader context, at least not yet.
-call :cplocal Kingdom.CodeAnalysis.Verification
-
-call :cplocal Kingdom.Collections.Enumerations.Attributes
-call :cplocal Kingdom.Collections.Enumerations.Analyzers
-call :cplocal Kingdom.Collections.Enumerations.Generators
-call :cplocal Kingdom.Collections.Enumerations.BuildTime
-
-call :cplocal Kingdom.CodeAnalysis.Verifiers.CodeFixes
-call :cplocal Kingdom.CodeAnalysis.Verifiers.Diagnostics
-
-call :cplocal Kingdom.Collections.ImmutableBitArray
-call :cplocal Kingdom.Collections.Enumerations
-call :cplocal Kingdom.Collections.Enumerations.Tests
-call :cplocal Kingdom.Collections.Stacks
-call :cplocal Kingdom.Collections.Queues
-call :cplocal Kingdom.Collections.Deques
+rem This is an interesting package for local consumption, but I do not think
+rem it should be published in a broader context, at least not yet.
+:next_project
+if defined projects (
+    for /f "tokens=1* delims=%delim%" %%p in ("%projects%") do (
+        rem Processing now as a function of input arguments.
+        call :copy_local %%p
+        set projects=%%q
+    )
+    goto :next_project
+)
 
 popd
 
 goto :end
 
-:cplocal
-set f=%1
-set package_path=%f%\bin\%config%\%f%.*%nupkg_ext%
-::set package_path=%f%.*%nupkg_ext%
+:copy_local
+pushd %1\bin\%config%
+set package_path=%1.*%nupkg_ext%
+rem set package_path=%1.*%nupkg_ext%
 if not exist "%package_path%" (
     echo Package '%package_path%' not found.
 )
-:: We need to scan beyond the wildcard package path here and get to specifics.
+rem We need to scan beyond the wildcard package path here and get to specifics.
 for %%x in ("%package_path%") do (
     if not exist "%local_dir%" mkdir "%local_dir%"
-    :: Make sure that we do not inadvertently overwrite already existing package versions.
+    rem Make sure that we do not inadvertently overwrite already existing package versions.
     if not exist "%local_dir%\%%x" echo Copying '%%x' package to local directory '%local_dir%'...
     if not exist "%local_dir%\%%x" xcopy /Y "%%x" "%local_dir%"
 )
+popd
 exit /b
 
 :end
