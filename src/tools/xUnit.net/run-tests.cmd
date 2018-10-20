43a703e6e8e78579f929ef0ead8c69b3a627ee1e
Michael W Powell <mwpowellhtx@gmail.com>
2018-10-20 14:54:37 -0400

    updated a couple of informative comments updated label naming conventions a bit updated condition conventions from empty string tests to defined and not defined conditions

--- src/tools/xUnit.net/run-tests.cmd
+++ src/tools/xUnit.net/run-tests.cmd
@@ -2,119 +2,118 @@
 
 setlocal
 
-rem TODO: TBD: do not keep this particular comment...
-rem TODO: TBD: apply my learnings concerning :: vs rem comments
-rem TODO: TBD: also apply my learnings re: for loop iteration over a range/list of delimited items
-rem TODO: TBD: apply this to my packaging scripts as well...
 rem TODO: TBD: the R# xUnit testing saga is as of this moment still unresolved
 rem TODO: TBD: specifically concerning treatment of user furnished xUnit settings
 rem TODO: TBD: working to move past this using Visual Studio Test Explorer, console runner, etc
 rem TODO: TBD: although it would be very good to have that capability in sound working order from the jetbrains folks sooner than later
 
-rem Projects are Comma (,) delimited.
-set delim=,
+rem Default project delimiter is the Comma (,).
+if not defined delim (
+    set delim=,
+)
+
 set projects=
 set xunit_opts=
 
-:parseargs
+:parse_args
 
 rem rem For debugging...
 rem echo %%1 = "%1"
 
 if "%1" == "" (
-    goto :endargs
+    goto :end_args
 )
 
 if "%1" == "--config" (
     shift
     rem echo config %2
     set config=%2
-    goto :nextarg
+    goto :next_arg
 )
 
 if "%1" == "--project" (
     shift
     rem echo project %2
-    if "%projects%" == "" (
+    if not defined projects (
         set projects=%2
     ) else (
         set projects=%projects%%delim%%2
     )
-    goto :nextarg
+    goto :next_arg
 )
 
 rem Now handle some xUnit.net specific options.
 if "%1" == "--verbose" (
     rem echo verbose
-    if "%xunit_opts%" == "" (
+    if not defined xunit_opts (
         set xunit_opts=-verbose
     ) else (
         set xunit_opts=%xunit_opts% -verbose
     )
-    goto :nextarg
+    goto :next_arg
 )
 
 if "%1" == "--parallel" (
     rem echo parallel %2
     set parallel_opts=none%delim%collections%delim%assemblies%delim%all
-:next_parallel
-    for /f "tokens=1* delims=%delim%" %%p in ("%parallel_opts%") do (
-        if "%2" == "%%p" (
-            if "%xunit_opts%" == "" (
-                set xunit_opts=-parallel %2
+:next_parallel_opt
+    if defined parallel_opts (
+        for /f "tokens=1* delims=%delim%" %%p in ("%parallel_opts%") do (
+            if "%2" == "%%p" (
+                if not defined xunit_opts (
+                    set xunit_opts=-parallel %2
+                ) else (
+                    set xunit_opts=%xunit_opts% -parallel %2
+                )
+                set parallel_opts=
             ) else (
-                set xunit_opts=%xunit_opts% -parallel %2
+                set parallel_opts=%%q
             )
-            set parallel_opts=
-        ) else (
-            set parallel_opts=%%q
+            goto :next_parallel_opt
         )
     )
-    if defined parallel_opts (
-        goto :next_parallel
-    )
     shift
-    goto :nextarg
+    goto :next_arg
 )
 
 if "%1" == "--xml" (
     shift
     rem echo xml %2
-    if "%xunit_opts%" == "" (
+    if not defined xunit_opts (
         set xunit_opts=-xml %2.xml
     ) else (
         set xunit_opts=%xunit_opts% -xml %2.xml
     )
-    goto :nextarg
+    goto :next_arg
 )
 
 if "%1" == "--stop-on-failure" (
     rem echo stop-on-failure
-    if "%xunit_opts%" == "" (
+    if not defined xunit_opts (
         set xunit_opts=-stoponfail
     ) else (
         set xunit_opts=%xunit_opts% -stoponfail
     )
-    goto :nextarg
+    goto :next_arg
 )
 
 if "%1" == "--fail-skips" (
     rem echo fail-skips
-    if "%xunit_opts%" == "" (
+    if not defined xunit_opts (
         set xunit_opts=-failskips
     ) else (
         set xunit_opts=%xunit_opts% -failskips
     )
-    goto :nextarg
+    goto :next_arg
 )
 
-:nextarg
+:next_arg
 
 shift
 
-goto :parseargs
+goto :parse_args
 
-:endargs
+:end_args
 
 rem TODO: TBD: may make these input arguments...
 
@@ -131,57 +130,55 @@ rem set xunit_prefix=dotnet run
 set xunit_bin=%xunit_bin_dir%\xunit.console%xunit_bin_ext%
 
 rem Do the main areas here.
-:testprojects
+:test_projects
 
 pushd ..\..
 
 rem Projects will have already been set, appended, etc.
-:nextproject
-if not "%projects%" == "" (
+:next_project
+if defined projects (
     for /f "tokens=1* delims=%delim%" %%p in ("%projects%") do (
         rem Processing now as a function of input arguments.
-        call :run %%p
+        call :test_one_project %%p
         set projects=%%q
     )
-)
-if defined projects (
-    goto :nextproject
+    goto :next_project
 )
 
 popd
 
 goto :end
 
-:run
+:test_one_project
 pushd %1\bin\%config%
 rem Re-set this one every time.
 set extensions=exe%delim%dll
-:nextext
+:next_ext
 rem Iterate the possible Extensions here.
-for /f "tokens=1* delims=%delim%" %%e in ("%extensions%") do (
-    set extensions=%%f
-    rem Then test for existence.
-    if exist "%1.%%e" (
-        echo Invoking console tests for "%1.%%e" ...
-        echo %xunit_prefix%"..\..\..\%xunit_bin%" "%1.%%e" %xunit_opts%
-        %xunit_prefix%"..\..\..\%xunit_bin%" "%1.%%e" %xunit_opts%
-        if errorlevel 0 (
-            echo All tests passed.
-        ) else if errorlevel 1 (
-            echo One or more tests failed.
-        ) else if errorlevel 3 (
-            echo Error with one or more xUnit arguments: %xunit_prefix% %xunit_bin% "%1.%%e".
-        ) else if errorlevel 4 (
-            echo Problem loading one or more assemblies during test: "%1\bin\%config%\%1.%%e".
-        ) else if errorlevel -1073741510 (
-            echo User canceled tests.
-        )
-    ) else (
-        echo Test artifact %1\bin\%config%\%1.%%e not found or has not yet been built.
-    )
-)
 if defined extensions (
-    goto :nextext
+    for /f "tokens=1* delims=%delim%" %%e in ("%extensions%") do (
+        set extensions=%%f
+        rem Then test for existence.
+        if exist "%1.%%e" (
+            echo Invoking console tests for "%1.%%e" ...
+            echo %xunit_prefix%"..\..\..\%xunit_bin%" "%1.%%e" %xunit_opts%
+            %xunit_prefix%"..\..\..\%xunit_bin%" "%1.%%e" %xunit_opts%
+            if errorlevel 0 (
+                echo All tests passed.
+            ) else if errorlevel 1 (
+                echo One or more tests failed.
+            ) else if errorlevel 3 (
+                echo Error with one or more xUnit arguments: %xunit_prefix% %xunit_bin% "%1.%%e".
+            ) else if errorlevel 4 (
+                echo Problem loading one or more assemblies during test: "%1\bin\%config%\%1.%%e".
+            ) else if errorlevel -1073741510 (
+                echo User canceled tests.
+            )
+        ) else (
+            echo Test artifact %1\bin\%config%\%1.%%e not found or has not yet been built.
+        )
+    )
+    goto :next_ext
 )
 popd
 exit /b
