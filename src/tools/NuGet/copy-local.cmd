88a15a0bb435597c00ecd36b219d754c524a5c17
Michael W Powell <mwpowellhtx@gmail.com>
2018-10-24 00:14:15 -0400

    made some corrections to the command batch files added a debug option for performing a dry run basically dry run does everything in the script short of the terminal command itself regardless whether that is a local copy or a publish package and so on make sure the else clauses are also correctly placed this was causing issues make sure we are scanning recursively as well

--- src/tools/NuGet/copy-local.cmd
+++ src/tools/NuGet/copy-local.cmd
@@ -12,9 +12,16 @@ rem set local_dir=C:\Dev\NuGet\packages
 set local_dir=G:\Dev\NuGet\local\packages
 set nupkg_ext=.nupkg
 
-rem Prepare for delimited list of Projects.
-if not defined delim (
+rem Default list delimiter is Comma (,).
+:redefine_delim
+if "%delim%" == "" (
     set delim=,
+)
+rem Redefine the delimiter when a Dot (.) is discovered.
+rem Anticipates potentially accepting Delimiter as a command line arg.
+if "%delim%" == "." (
+    set delim=
+    goto :redefine_delim
 )
 
 :set_projects
@@ -33,6 +40,10 @@ set all_projects=%all_projects%%delim%Kingdom.Collections.Enumerations.Tests
 set all_projects=%all_projects%%delim%Kingdom.Collections.Stacks
 set all_projects=%all_projects%%delim%Kingdom.Collections.Queues
 set all_projects=%all_projects%%delim%Kingdom.Collections.Deques
+rem Setup Collections Projects
+set collections_projects=%collections_projects%%delim%Kingdom.Collections.Stacks
+set collections_projects=%collections_projects%%delim%Kingdom.Collections.Queues
+set collections_projects=%collections_projects%%delim%Kingdom.Collections.Deques
 rem Setup Enumerations Projects
 set enum_projects=Kingdom.Collections.ImmutableBitArray
 set enum_projects=%enum_projects%%delim%Kingdom.Collections.Enumerations.Attributes
@@ -44,7 +55,7 @@ set enum_projects=%enum_projects%%delim%Kingdom.CodeAnalysis.Verifiers.CodeFixes
 set enum_projects=%enum_projects%%delim%Kingdom.CodeAnalysis.Verifiers.Diagnostics
 set enum_projects=%enum_projects%%delim%Kingdom.Collections.Enumerations
 set enum_projects=%enum_projects%%delim%Kingdom.Collections.Enumerations.Tests
-rem Setup Bit Array
+rem Setup Bit Array Projects
 set bit_array_projects=Kingdom.Collections.ImmutableBitArray
 
 :parse_args
@@ -53,69 +64,97 @@ if "%1" == "" (
     goto :end_args
 )
 
+:set_dry_run
+if "%1" == "--dry" (
+    set dry=true
+    goto :next_arg
+)
+if "%1" == "--dry-run" (
+    set dry=true
+    goto :next_arg
+)
+
 :set_config
+rem echo set_config = %1
 if "%1" == "--config" (
     set config=%2
     shift
-    goto :next_args
+    goto :next_arg
+)
+
+:add_enum_projects
+rem echo add_enum_projects = %1
+if "%1" == "--enums" (
+    if "%projects%" == "" (
+        set projects=%enum_projects%
+    ) else (
+        set projects=%projects%%delim%%enum_projects%
+    )
+	goto :next_arg
+)
+
+if "%1" == "--enumerations" (
+    if "%projects%" == "" (
+        set projects=%enum_projects%
+    ) else (
+        set projects=%projects%%delim%%enum_projects%
+    )
+	goto :next_arg
+)
+
+:add_bit_array_projects
+rem echo add_bit_array_projects = %1
+if "%1" == "--bits" (
+    if "%projects%" == "" (
+        set projects=%bit_array_projects%
+    ) else (
+        set projects=%projects%%delim%%bit_array_projects%
+    )
+	goto :next_arg
+)
+
+if "%1" == "--bit-array" (
+    if "%projects%" == "" (
+        set projects=%bit_array_projects%
+    ) else (
+        set projects=%projects%%delim%%bit_array_projects%
+    )
+	goto :next_arg
+)
+
+:add_collections_projects
+rem echo add_collections_projects = %1
+if "%1" == "--collections" (
+    rem Prepare to publish Collections Projects.
+    if "%projects%" == "" (
+        set projects=%collections_projects%
+    ) else (
+        set projects=%projects%%delim%%collections_projects%
+    )
+    goto :next_arg
+)
+
+:add_all_projects
+rem echo add_all_projects = %1
+if "%1" == "--all" (
+    set projects=%all_projects%
+	goto :next_arg
 )
 
 :add_project
+rem echo add_project = %1
 if "%1" == "--project" (
-    if not defined projects (
+    if "%projects%" == "" (
         set projects=%2
     ) else (
         set projects=%projects%%delim%%2
     )
     shift
-    goto :next_args
+    goto :next_arg
 )
 
-:add_all_projects
-if "%1" == "--all" (
-    set projects=%all_projects%
-	goto :next_args
-)
-
-:add_enum_projects
-if "%1" == "--enums" (
-    if not defined projects (
-        set projects=%enum_projects%
-    ) else (
-        set projects=%projects%%delim%%enum_projects%
-    )
-	goto :next_args
-)
-if "%1" == "--enumerations" (
-    if not defined projects (
-        set projects=%enum_projects%
-    ) else (
-        set projects=%projects%%delim%%enum_projects%
-    )
-	goto :next_args
-)
-
-:add_bit_array_projects
-if "%1" == "--bits" (
-    if not defined projects (
-        set projects=%bit_array_projects%
-    ) else (
-        set projects=%projects%%delim%%bit_array_projects%
-    )
-	goto :next_args
-)
-if "%1" == "--bit-array" (
-    if not defined projects (
-        set projects=%bit_array_projects%
-    ) else (
-        set projects=%projects%%delim%%bit_array_projects%
-    )
-	goto :next_args
-)
-
-:next_args
+:next_arg
 shift
-
 goto :parse_args
 
 :end_args
@@ -123,13 +162,14 @@ goto :parse_args
 :verify_args
 
 :verify_projects
-if not defined projects (
+if "%projects%" == "" (
     rem In which case, there is nothing else to do.
+    echo Nothing to process.
     goto :end
 )
 
 :verify_config
-if not defined config (
+if "%config%" == "" (
     set config=Release
 )
 
@@ -139,8 +179,11 @@ pushd ..\..
 
 rem This is an interesting package for local consumption, but I do not think
 rem it should be published in a broader context, at least not yet.
+if not "%projects%" == "" (
+    echo Processing '%config%' configuration for '%projects%' ...
+)
 :next_project
-if defined projects (
+if not "%projects%" == "" (
     for /f "tokens=1* delims=%delim%" %%p in ("%projects%") do (
         rem Processing now as a function of input arguments.
         call :copy_local %%p
@@ -155,18 +198,27 @@ goto :end
 
 :copy_local
 pushd %1\bin\%config%
+rem Because Package may exist in the %config%\%TargetFramework% ...
 set package_path=%1.*%nupkg_ext%
-rem set package_path=%1.*%nupkg_ext%
-if not exist "%package_path%" (
-    echo Package '%package_path%' not found.
-)
 rem We need to scan beyond the wildcard package path here and get to specifics.
-for %%x in ("%package_path%") do (
-    if not exist "%local_dir%" mkdir "%local_dir%"
-    rem Make sure that we do not inadvertently overwrite already existing package versions.
-    if not exist "%local_dir%\%%x" echo Copying '%%x' package to local directory '%local_dir%'...
-    if not exist "%local_dir%\%%x" xcopy /Y "%%x" "%local_dir%"
+for /r %%x in ("%package_path%") do (
+    if not exist "%%x" (
+        goto :copy_not_found
+    )
+    if "%dry%" == "true" (
+        echo Set to copy '%%x' to '%local_dir%' ...
+    ) else (
+        echo Wet run '%%x' ...
+        rem if not exist "%local_dir%" mkdir "%local_dir%"
+        rem rem Make sure that we do not inadvertently overwrite already existing package versions.
+        rem if not exist "%local_dir%\%%x" echo Copying '%%x' package to local directory '%local_dir%'...
+        rem if not exist "%local_dir%\%%x" xcopy /Y "%%x" "%local_dir%"
+    )
+    goto :end_copy
 )
+:copy_not_found
+echo Package '%package_path%' not found.
+:end_copy
 popd
 exit /b
 
--- src/tools/NuGet/publish.cmd
+++ src/tools/NuGet/publish.cmd
@@ -10,7 +10,7 @@ if "%my_nuget_api_key%"=="" (
 
 rem Default list delimiter is Comma (,).
 :redefine_delim
-if not defined delim (
+if "%delim%" == "" (
     set delim=,
 )
 rem Redefine the delimiter when a Dot (.) is discovered.
@@ -21,9 +21,9 @@ if "%delim%" == "." (
 )
 
 rem Go ahead and pre-seed the Projects up front.
-:set_all_projects
+:set_projects
 set projects=
-rem All Projects
+rem Setup All Projects
 set all_projects=Kingdom.Collections.ImmutableBitArray
 set all_projects=%all_projects%%delim%Kingdom.Collections.Enumerations.Attributes
 set all_projects=%all_projects%%delim%Kingdom.Collections.Enumerations.Analyzers
@@ -37,7 +37,11 @@ set all_projects=%all_projects%%delim%Kingdom.Collections.Enumerations.Tests
 set all_projects=%all_projects%%delim%Kingdom.Collections.Stacks
 set all_projects=%all_projects%%delim%Kingdom.Collections.Queues
 set all_projects=%all_projects%%delim%Kingdom.Collections.Deques
-rem Enumerations Projects
+rem Setup Collections Projects
+set collections_projects=%collections_projects%%delim%Kingdom.Collections.Stacks
+set collections_projects=%collections_projects%%delim%Kingdom.Collections.Queues
+set collections_projects=%collections_projects%%delim%Kingdom.Collections.Deques
+rem Setup Enumerations Projects
 set enum_projects=Kingdom.Collections.ImmutableBitArray
 set enum_projects=%enum_projects%%delim%Kingdom.Collections.Enumerations.Attributes
 set enum_projects=%enum_projects%%delim%Kingdom.Collections.Enumerations.Analyzers
@@ -48,17 +52,25 @@ set enum_projects=%enum_projects%%delim%Kingdom.CodeAnalysis.Verifiers.CodeFixes
 set enum_projects=%enum_projects%%delim%Kingdom.CodeAnalysis.Verifiers.Diagnostics
 set enum_projects=%enum_projects%%delim%Kingdom.Collections.Enumerations
 set enum_projects=%enum_projects%%delim%Kingdom.Collections.Enumerations.Tests
-set enum_projects=%enum_projects%%delim%Kingdom.Collections.Stacks
-set enum_projects=%enum_projects%%delim%Kingdom.Collections.Queues
-set enum_projects=%enum_projects%%delim%Kingdom.Collections.Deques
-rem Bit Array Projects
+rem Setup Bit Array Projects
 set bit_array_projects=Kingdom.Collections.ImmutableBitArray
+
 
 :parse_args
 
 rem Done parsing the args.
 if "%1" == "" (
     goto :end_args
+)
+
+:set_dry_run
+if "%1" == "--dry" (
+    set dry=true
+    goto :next_arg
+)
+if "%1" == "--dry-run" (
+    set dry=true
+    goto :next_arg
 )
 
 :set_config
@@ -68,15 +80,56 @@ if "%1" == "--config" (
     goto :next_arg
 )
 
-:add_project
-if "%1" == "--project" (
-    rem Add a Project to the Projects list.
-    if not defined projects (
-        set projects=%2
+:add_enum_projects
+if "%1" == "--enums" (
+    rem Prepare to publish Enumerations Projects.
+    if "%projects%" == "" (
+        set projects=%enum_projects%
     ) else (
-        set projects=%projects%%delim%%2
+        set projects=%projects%%delim%%enum_projects%
     )
-    shift
+    goto :next_arg
+)
+
+if "%1" == "--enumerations" (
+    rem Prepare to publish Enumerations Projects.
+    if "%projects%" == "" (
+        set projects=%enum_projects%
+    ) else (
+        set projects=%projects%%delim%%enum_projects%
+    )
+    goto :next_arg
+)
+
+:add_bit_array_projects
+if "%1" == "--bits" (
+    rem Prepare to publish Bit Array Projects.
+    if "%projects%" == "" (
+        set projects=%bit_array_projects%
+    ) else (
+        set projects=%projects%%delim%%bit_array_projects%
+    )
+    goto :next_arg
+)
+
+if "%1" == "--bit-array" (
+    rem Prepare to publish Bit Array Projects.
+    if "%projects%" == "" (
+        set projects=%bit_array_projects%
+    ) else (
+        set projects=%projects%%delim%%bit_array_projects%
+    )
+    goto :next_arg
+)
+
+:add_collections_projects
+if "%1" == "--collections" (
+    rem Prepare to publish Collections Projects.
+    if "%projects%" == "" (
+        set projects=%collections_projects%
+    ) else (
+        set projects=%projects%%delim%%collections_projects%
+    )
     goto :next_arg
 )
 
@@ -87,47 +140,15 @@ if "%1" == "--all" (
     goto :next_arg
 )
 
-:add_enum_projects
-if "%1" == "--enums" (
-    rem Prepare to publish Enumerations Projects.
-    if not defined projects (
-        set projects=%enum_projects%
+:add_project
+if "%1" == "--project" (
+    rem Add a Project to the Projects list.
+    if "%projects%" == "" (
+        set projects=%2
+    ) else (
+        set projects=%projects%%delim%%2
     )
-    else (
-        set projects=%projects%%delim%%enum_projects%
-    )
-    goto :next_arg
-)
-if "%1" == "--enumerations" (
-    rem Prepare to publish Enumerations Projects.
-    if not defined projects (
-        set projects=%enum_projects%
-    )
-    else (
-        set projects=%projects%%delim%%enum_projects%
-    )
-    goto :next_arg
-)
-
-:add_bit_array_projects
-if "%1" == "--bit" (
-    rem Prepare to publish Bit Array Projects.
-    if not defined projects (
-        set projects=%bit_array_projects%
-    )
-    else (
-        set projects=%projects%%delim%%bit_array_projects%
-    )
-    goto :next_arg
-)
-if "%1" == "--bit-array" (
-    rem Prepare to publish Bit Array Projects.
-    if not defined projects (
-        set projects=%bit_array_projects%
-    )
-    else (
-        set projects=%projects%%delim%%bit_array_projects%
-    )
+    shift
     goto :next_arg
 )
 
@@ -142,7 +163,7 @@ goto :parse_args
 :verify_args
 
 :verify_config
-if not defined config (
+if "%config%" == "" (
     rem Assumes Release Configuration when not otherwise specified.
     set config=Release
 )
@@ -163,12 +184,13 @@ set nuget_push_opts=%nuget_push_opts% -Source %nuget_push_source%
 rem Do the main areas here.
 pushd ..\..
 
+if not "%projects%" == "" (
+    echo Processing '%config%' configuration for '%projects%' ...
+)
 :next_project
-if defined projects (
+if not "%projects%" == "" (
     for /f "tokens=1* delims=%delim%" %%p in ("%projects%") do (
-        if not "%%p" == "" (
-            call :publish_pkg %%p
-        )
+        call :publish_pkg %%p
         set projects=%%q
         goto :next_project
     )
@@ -180,7 +202,12 @@ goto :end
 
 :publish_pkg
 for %%f in ("%1\bin\%config%\%1.*.nupkg") do (
-    %nuget_exe% push "%%f" %nuget_push_opts%
+    if "%dry%" == "true" (
+        echo Dry run: %nuget_exe% push "%%f"%nuget_push_opts%
+    ) else (
+        echo Running: %nuget_exe% push "%%f"%nuget_push_opts%
+        %nuget_exe% push "%%f"%nuget_push_opts%
+    )
 )
 exit /b
 
