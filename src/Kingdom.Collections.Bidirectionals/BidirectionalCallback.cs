929b21c95741c16989394585ff5a16902ca2be26
Michael W Powell <mwpowellhtx@gmail.com>
2019-02-26 18:08:23 -0500

    bidirectional initial commitment

---
+++ src/Kingdom.Collections.Bidirectionals/BidirectionalCallback.cs
@@ -0,0 +1,9 @@
+﻿namespace Kingdom.Collections
+{
+    /// <summary>
+    /// Callback when either Adding or Removing in any way, shape, or form.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="item"></param>
+    public delegate void BidirectionalCallback<in T>(T item);
+}
---
+++ src/Kingdom.Collections.Bidirectionals/IBidirectionalList.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace Kingdom.Collections
+{
+    /// <summary>
+    /// Bidirectional List interface.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <inheritdoc />
+    public interface IBidirectionalList<T> : IList<T>
+    {
+        /// <summary>
+        /// Event raised prior to Adding an Item.
+        /// </summary>
+        event BidirectionalCallback<T> AddingItem;
+
+        /// <summary>
+        /// Event raised after an Item has been Added.
+        /// </summary>
+        event BidirectionalCallback<T> AddedItem;
+
+        /// <summary>
+        /// Event raised prior to Removing an Item.
+        /// </summary>
+        event BidirectionalCallback<T> RemovingItem;
+
+        /// <summary>
+        /// Event raised after an Item has been Removed.
+        /// </summary>
+        event BidirectionalCallback<T> RemovedItem;
+    }
+}
