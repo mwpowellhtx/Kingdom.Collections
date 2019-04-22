a1786e6014567a34142889ee63406a2d1ef796d9
Michael W Powell <mwpowellhtx@gmail.com>
2019-04-22 17:17:40 -0400

    variants initial commitment

---
+++ src/Kingdom.Collections.Variants.Tests/Bases/TestFixtureBase.cs
@@ -0,0 +1,14 @@
+﻿namespace Kingdom.Collections.Variants
+{
+    using Xunit.Abstractions;
+
+    public abstract class TestFixtureBase
+    {
+        protected ITestOutputHelper OutputHelper { get; }
+
+        protected TestFixtureBase(ITestOutputHelper outputHelper)
+        {
+            OutputHelper = outputHelper;
+        }
+    }
+}
