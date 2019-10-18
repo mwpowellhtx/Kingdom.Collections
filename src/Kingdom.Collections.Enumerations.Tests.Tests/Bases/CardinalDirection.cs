780a2fff058c266ab4f7afd51f3b2cb10118339e
Michael W. Powell <mwpowelllde@gmail.com>
2019-10-18 09:59:19 -0400

    updated enumerations tests with latest draft efforts

---
+++ src/Kingdom.Collections.Enumerations.Tests.Tests/Bases/CardinalDirection.cs
@@ -0,0 +1,27 @@
+﻿namespace Kingdom.Collections.Bases
+{
+    public class CardinalDirection : BaseEnumeration<CardinalDirection>
+    {
+        protected internal override string DebuggerDisplayName => DisplayName;
+
+        public static readonly CardinalDirection North = new CardinalDirection();
+
+        public static readonly CardinalDirection NorthWest = new CardinalDirection();
+
+        public static readonly CardinalDirection West = new CardinalDirection();
+
+        public static readonly CardinalDirection SouthWest = new CardinalDirection();
+
+        public static readonly CardinalDirection South = new CardinalDirection();
+
+        public static readonly CardinalDirection SouthEast = new CardinalDirection();
+
+        public static readonly CardinalDirection East = new CardinalDirection();
+
+        public static readonly CardinalDirection NorthEast = new CardinalDirection();
+
+        private CardinalDirection()
+        {
+        }
+    }
+}
--- src/Kingdom.Collections.Enumerations.Tests.Tests/Properties/AssemblyInfo.cs
+++ src/Kingdom.Collections.Enumerations.Tests.Tests/Properties/AssemblyInfo.cs
@@ -1,29 +1,29 @@
-using System.Reflection;
-using System.Runtime.InteropServices;
+//using System.Reflection;
+//using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Kingdom.Collections.Enumerations.Tests.Tests")]
-[assembly: AssemblyDescription("")]
+//// General Information about an assembly is controlled through the following 
+//// set of attributes. Change these attribute values to modify the information
+//// associated with an assembly.
+//[assembly: AssemblyTitle("Kingdom.Collections.Enumerations.Tests.Tests")]
+//[assembly: AssemblyDescription("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+//// Setting ComVisible to false makes the types in this assembly not visible 
+//// to COM components.  If you need to access a type in this assembly from 
+//// COM, set the ComVisible attribute to true on that type.
+//[assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("81f0159a-e3c3-4354-859c-62fc68d05882")]
+//// The following GUID is for the ID of the typelib if this project is exposed to COM
+//[assembly: Guid("81f0159a-e3c3-4354-859c-62fc68d05882")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.1.2594")]
-[assembly: AssemblyFileVersion("1.1.1.2594")]
+//// Version information for an assembly consists of the following four values:
+////
+////      Major Version
+////      Minor Version 
+////      Build Number
+////      Revision
+////
+//// You can specify all the values or you can default the Build and Revision Numbers 
+//// by using the '*' as shown below:
+//// [assembly: AssemblyVersion("1.0.*")]
+//[assembly: AssemblyVersion("1.1.1.2597")]
+//[assembly: AssemblyFileVersion("1.1.1.2594")]
